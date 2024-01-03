f620f48fadfadeeb023eb48f0f96117184682a44
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-01-03 11:58:24 +0600

    initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return <section style={{
+    height: "100vh",
+    display: 'flex',
+    alignItems: "center",
+    justifyContent: 'center'
+  }}>
+    <img src="/foxtream.png"
+      alt="foxtream"
+      style={{ width: "150px" }} />
+  </section>
+}
