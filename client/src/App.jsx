1bdb99d1efde39505316e357ad141571d614e036
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-01-10 09:33:52 +0600

    loading, scroll & error-404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Navbar from "./layouts/Navbar";
 import Streaming from "./pages/Streaming";
 import Search from "./pages/Search";
+import E404 from "./pages/E404";
 
 export default function App() {
   return <BrowserRouter>
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/streaming/:animeId" element={<Streaming />} />
       <Route path="/search/:query" element={<Search />} />
+      <Route path="*" element={<E404 />} />
     </Routes>
   </BrowserRouter>
 }
--- client/src/pages/Streaming.jsx
+++ client/src/pages/Streaming.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ReactPlayer from 'react-player';
+import Loading from "../components/Loading";
 
 export default function Streaming() {
     const { animeId } = useParams();
@@ -9,6 +10,7 @@ export default function Streaming() {
     const [episodeId, setEpisodeId] = useState(animeId + '-episode-1');
     const [episodeUrl, setEpisodeUrl] = useState('');
     const [episodes, setEpisodes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const rebumpEpisode = async () => {
         const request = await fetch(`https://foxtream.up.railway.app/anime/gogoanime/info/${animeId}`)
@@ -24,6 +26,7 @@ export default function Streaming() {
             const request = await fetch(`https://foxtream.up.railway.app/anime/gogoanime/watch/${episodeId}`);
             const response = await request.json();
             if (request.status === 200) {
+                setLoading(false);
                 if (response.sources.length <= 4) {
                     setEpisodeUrl(response.sources[1].url);
                 } else {
@@ -33,6 +36,7 @@ export default function Streaming() {
                 rebumpEpisode();
             }
         } catch (error) {
+            setLoading(true);
             console.log(error.message);
         }
     }
@@ -55,73 +59,75 @@ export default function Streaming() {
         getAnimeInfo();
     }, [animeId])
     return (
-        <section className="container streaming">
-            <div className="streamingPlayer">
-                {episodeUrl && <div>
-                    <div className="streamingPlayerEpisodeNumber">
-                        {`${String(episodeId).split('-').slice(-2)[0]} ${String(episodeId).split('-').slice(-2)[1]}`}</div>
-                    {episodeUrl ? <ReactPlayer
-                        width="100%"
-                        height="auto"
-                        controls={true}
-                        playing={true}
-                        url={episodeUrl} /> : <p>Loading...</p>}
-                    <div className="streamingPlayerButtons">
-                        {episodes.map((epis, i) => {
-                            return <button
-                                onClick={() => { getEpisode(epis.id); setEpisodeId(epis.id) }}
-                                className="streamingPlayerButton"
-                                key={i}>{i + 1}
-                            </button>
-                        })}
-                    </div>
-                </div>}
-            </div>
-            <div className="streamingInfo">
-                {animeInfo && <div>
-                    <div className="imageStreaming">
-                        <div className="streaminImageContainer">
-                            {animeInfo.image && <img src={animeInfo.image}
-                                alt={animeId}
-                                className="streamingInfoPoster"
-                                draggable="false" />}
+        <>
+            {loading ? <Loading /> : <section className="container streaming">
+                <div className="streamingPlayer">
+                    {episodeUrl && <div>
+                        <div className="streamingPlayerEpisodeNumber">
+                            {`${String(episodeId).split('-').slice(-2)[0]} ${String(episodeId).split('-').slice(-2)[1]}`}</div>
+                        {episodeUrl && <ReactPlayer
+                            width="100%"
+                            height="auto"
+                            controls={true}
+                            playing={true}
+                            url={episodeUrl} />}
+                        <div className="streamingPlayerButtons">
+                            {episodes.map((epis) => {
+                                return <button
+                                    onClick={() => { getEpisode(epis.id); setEpisodeId(epis.id) }}
+                                    className="streamingPlayerButton"
+                                    key={epis.number}>{epis.number}
+                                </button>
+                            })}
                         </div>
-                        <div>
-                            <p>{animeInfo.title}</p>
-                            <p className="streamingInfoDescription">
-                                {String(animeInfo.description).length > 250 ? animeInfo.description.slice(0, 250) + "..." : animeInfo.description}
-                            </p>
+                    </div>}
+                </div>
+                <div className="streamingInfo">
+                    {animeInfo && <div>
+                        <div className="imageStreaming">
+                            <div className="streaminImageContainer">
+                                {animeInfo.image && <img src={animeInfo.image}
+                                    alt={animeId}
+                                    className="streamingInfoPoster"
+                                    draggable="false" />}
+                            </div>
+                            <div>
+                                <p>{animeInfo.title}</p>
+                                <p className="streamingInfoDescription">
+                                    {String(animeInfo.description).length > 250 ? animeInfo.description.slice(0, 250) + "..." : animeInfo.description}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="streamingInfoDeepAll">
-                        <div className="streamingInfoDeep">
-                            <p>Status</p>
-                            <p>{animeInfo.status}</p>
+                        <div className="streamingInfoDeepAll">
+                            <div className="streamingInfoDeep">
+                                <p>Status</p>
+                                <p>{animeInfo.status}</p>
+                            </div>
+                            <div className="streamingInfoDeep">
+                                <p>Type</p>
+                                <p>{animeInfo.type}</p>
+                            </div>
+                            <div className="streamingInfoDeep">
+                                <p>Episodes</p>
+                                <p>{animeInfo.totalEpisodes}</p>
+                            </div>
+                            <div className="streamingInfoDeep">
+                                <p>Released</p>
+                                <p>{animeInfo.releaseDate}</p>
+                            </div>
+                            <div className="streamingInfoDeep">
+                                <p>Other Names</p>
+                                <p>{animeInfo.otherName}</p>
+                            </div>
                         </div>
-                        <div className="streamingInfoDeep">
-                            <p>Type</p>
-                            <p>{animeInfo.type}</p>
+                        <div className="streamingGenres">
+                            {genres && genres.map((genre, i) => {
+                                return <p key={i} className="streamingGenre">{genre}</p>
+                            })}
                         </div>
-                        <div className="streamingInfoDeep">
-                            <p>Episodes</p>
-                            <p>{animeInfo.totalEpisodes}</p>
-                        </div>
-                        <div className="streamingInfoDeep">
-                            <p>Released</p>
-                            <p>{animeInfo.releaseDate}</p>
-                        </div>
-                        <div className="streamingInfoDeep">
-                            <p>Other Names</p>
-                            <p>{animeInfo.otherName}</p>
-                        </div>
-                    </div>
-                    <div className="streamingGenres">
-                        {genres && genres.map((genre, i) => {
-                            return <p key={i} className="streamingGenre">{genre}</p>
-                        })}
-                    </div>
-                </div>}
-            </div>
-        </section>
+                    </div>}
+                </div>
+            </section>}
+        </>
     )
 }
