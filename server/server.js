ff06bcfa6410f99e0fe9a364f380093d4718dc06
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-01-16 18:08:18 +0600

    cors-fix

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://foxtream.vercel.app", "http://localhost:5173"],
     methods: "*",
   })
 );
