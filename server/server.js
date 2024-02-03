39843180470032018b1305c863eb2839acae9266
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-02-04 00:01:34 +0600

    favicon

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://komachi-chan.vercel.app"],
+    origin: ["https://komachi-chan.vercel.app", "http://localhost:5173"],
     methods: "*",
   })
 );
