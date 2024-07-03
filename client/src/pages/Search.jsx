0cb366686d5df3c99fe4a7fc8a3fa20afeb31229
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2024-07-04 01:46:10 +0600

    feat: search/format works (#29)

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { useParams, NavLink, useLocation, Link } from "react-router-dom";
 import { useAuth } from "../store/auth.jsx";
 import { Helmet } from "react-helmet";
@@ -7,33 +7,56 @@ import styles from "../styles/search.module.css";
 // ICONS
 import { RiSearchLine } from "react-icons/ri";
 import { IoChevronDown } from "react-icons/io5";
-import { MdTune } from "react-icons/md";
+import { MdTune, MdVerified } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+// Components
 import Card from "../components/Card.jsx";
 
 export default function Search() {
   const { SERVER } = useAuth();
   const { search } = useLocation();
   const location = new URLSearchParams(search);
-  const [query, setQuery] = useState(location.get("query"));
+  const [query, setQuery] = useState(
+    location.get("query") ? location.get("query") : ""
+  );
   const [searched, setSearched] = useState([]);
   const [trending, setTrending] = useState([]);
   const [popular, setPopular] = useState([]);
+  // Advance Search Open/Close
+  const [isFormatOpen, setIsFormatOpen] = useState(false);
+  // Advance Search States
+  const [year, setYear] = useState(undefined);
+  const [season, setSeason] = useState(undefined);
+  const [format, setFormat] = useState([]);
+  const [genres, setGenres] = useState([]);
+  // References
+  const formatRef = useRef(null);
+
+  // for format ouside click
+  const handleClickOutside = (event) => {
+    if (event.target.className !== "format_btn") {
+      setIsFormatOpen(false);
+    }
+  };
 
   const getSearched = async (e) => {
-    // e.preventDefault();
-    if (query) {
-      const request = await fetch(`${SERVER}/api/v1/anime/advance-search`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ query }),
-      });
-      const response = await request.json();
-      // console.log(response);
-      if (request.status === 200) {
-        setSearched(response);
-      } else {
-        console.log(response);
-      }
+    const request = await fetch(`${SERVER}/api/v1/anime/advance-search`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query,
+        format:
+          format.length > 0
+            ? format
+            : ["TV", "TV_SHORT", "SPECIAL", "MOVIE", "OVA", "ONA"],
+      }),
+    });
+    const response = await request.json();
+    // console.log(response);
+    if (request.status === 200) {
+      setSearched(response);
+    } else {
+      console.log(response);
     }
   };
 
@@ -75,14 +98,28 @@ export default function Search() {
     }
   };
 
+  function insert_Into_Array(toInsertString, arrayToBeInsertedTo) {
+    setFormat((prevFormat) => {
+      if (!prevFormat.includes(toInsertString)) {
+        return [...prevFormat, toInsertString];
+      }
+      return prevFormat;
+    });
+  }
+
   useEffect(() => {
     getTrending();
     getPopular();
+    // Outside click handler
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
   }, []); // need to fetch only once
 
   useEffect(() => {
     getSearched();
-  }, [query]);
+  }, [query, format]);
   return (
     <section className="container">
       <Helmet>
@@ -135,12 +172,95 @@ export default function Search() {
             <IoChevronDown />
           </button>
         </section>
-        <section>
+        <section className={styles.list_Wrapper}>
           <p className={styles.selector_Title}>Format</p>
-          <button className={styles.selector}>
-            <p>Any</p>
+          <button
+            className={styles.selector}
+            onClick={() => {
+              setIsFormatOpen(true);
+            }}
+          >
+            <section ref={formatRef} className={styles.arrays}>
+              {format.length > 0 ? (
+                format.map((frmt, index) => {
+                  return (
+                    <div
+                      onClick={(e) => {
+                        e.stopPropagation(); // this stops it from closing dropdown
+                        //  delete format from list
+                        setFormat(format.filter((item) => item !== frmt));
+                      }}
+                      key={index}
+                      className={styles.array}
+                    >
+                      <p>{frmt}</p>
+                    </div>
+                  );
+                })
+              ) : (
+                <p>Any</p>
+              )}
+            </section>
             <IoChevronDown />
           </button>
+          {isFormatOpen && (
+            <section className={styles.genre_List}>
+              <button
+                className="format_btn"
+                onClick={() => {
+                  insert_Into_Array("TV", format);
+                }}
+              >
+                TV Show
+                {format.includes("TV") && <FaCheckCircle />}
+              </button>
+              <button
+                className="format_btn"
+                onClick={() => {
+                  insert_Into_Array("TV_SHORT", format);
+                }}
+              >
+                TV Short
+                {format.includes("TV_SHORT") && <FaCheckCircle />}
+              </button>
+              <button
+                className="format_btn"
+                onClick={() => {
+                  insert_Into_Array("MOVIE", format);
+                }}
+              >
+                Movie
+                {format.includes("MOVIE") && <FaCheckCircle />}
+              </button>
+              <button
+                className="format_btn"
+                onClick={() => {
+                  insert_Into_Array("SPECIAL", format);
+                }}
+              >
+                Special
+                {format.includes("SPECIAL") && <FaCheckCircle />}
+              </button>
+              <button
+                className="format_btn"
+                onClick={() => {
+                  insert_Into_Array("OVA", format);
+                }}
+              >
+                OVA
+                {format.includes("OVA") && <FaCheckCircle />}
+              </button>
+              <button
+                className="format_btn"
+                onClick={() => {
+                  insert_Into_Array("ONA", format);
+                }}
+              >
+                ONA
+                {format.includes("ONA") && <FaCheckCircle />}
+              </button>
+            </section>
+          )}
         </section>
         <section>
           <button className={styles.options}>{<MdTune />}</button>
