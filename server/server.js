aa076c9adaeea6d30b7dbf06afa44beb4e36984f
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-07-16 21:31:56 +0600

    fix: vercelll

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import database from "./utils/database.js";
 
 config();
 database();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(
