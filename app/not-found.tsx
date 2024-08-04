4567911d2ef030d6ca8c61211a5200849b3f08a3
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-08-04 10:54:49 +0600

    fix: conflict on not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,17 +6,10 @@ export default function NotFound() {
     <>
       <Navbar />
       <div className="e404">
-<<<<<<< HEAD
         <h1>Who&#39;s there!?</h1>
         <img width={200} draggable="false" src="/angry_chan.png" alt="e404" />
         <h1>Look where you are going.</h1>
         <h1>Nothing&#39;s here. Go back.</h1>
-=======
-        <h1>Who's there!?</h1>
-        <img width={200} draggable="false" src="/angry_chan.png" alt="e404" />
-        <h1>Look where are you going.</h1>
-        <h1>Nothings here. Go back.</h1>
->>>>>>> 8157361bd5b72498b01ccb61b20cdbb05c2a5f87
       </div>
       <Footer />
     </>
