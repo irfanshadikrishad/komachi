83cae63e64ec82f4887bc890f6f35252254ee739
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-08-04 11:18:54 +0600

    test error

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import styles from "@/styles/search.module.css";
@@ -24,9 +24,7 @@ export default function Search() {
       const request = await fetch(`/api/search`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          query,
-        }),
+        body: JSON.stringify({ query }),
       });
       const response = await request.json();
 
@@ -79,115 +77,101 @@ export default function Search() {
   useEffect(() => {
     getTrending();
     getPopular();
-  }, []); // need to fetch only once
+  }, []);
 
   useEffect(() => {
     getSearched();
   }, [query]);
+
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Navbar />
-        <section className="container">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-            className={styles.filter}
-          >
-            <section>
-              <p className={styles.selector_Title}>Search</p>
-              <div className={styles.searchFieldContainer}>
-                <RiSearchLine />
-                <input
-                  value={query ? query : ""}
-                  onChange={(e) => {
-                    setQuery(e.target.value);
-                  }}
-                  type="text"
-                  className={styles.searchField}
-                />
-              </div>
-            </section>
-          </form>
+      <Navbar />
+      <section className="container">
+        <form onSubmit={(e) => e.preventDefault()} className={styles.filter}>
+          <section>
+            <p className={styles.selector_Title}>Search</p>
+            <div className={styles.searchFieldContainer}>
+              <RiSearchLine />
+              <input
+                value={query ? query : ""}
+                onChange={(e) => setQuery(e.target.value)}
+                type="text"
+                className={styles.searchField}
+              />
+            </div>
+          </section>
+        </form>
 
-          {searched.length > 0 ? (
-            <section className={styles.billboard_Wrapper}>
-              <section className={styles.billboard_Main}>
-                <div className={styles.billboard_HeaderMain}>
-                  <p className={styles.billboard_Header}>
-                    {query !== null && `Search result for '${query}'`}
-                  </p>
-                </div>
-                <section className={styles.billboard}>
-                  {searched.map(
-                    ({ isAdult, anilistId, poster, totalEpisodes, title }) => {
-                      return (
-                        <Card
-                          key={anilistId}
-                          id={anilistId}
-                          image={poster}
-                          totalEpisodes={totalEpisodes}
-                          title={title}
-                          isAdult={isAdult}
-                        />
-                      );
-                    }
-                  )}
-                </section>
+        {searched.length > 0 ? (
+          <section className={styles.billboard_Wrapper}>
+            <section className={styles.billboard_Main}>
+              <div className={styles.billboard_HeaderMain}>
+                <p className={styles.billboard_Header}>
+                  {query !== null && `Search result for '${query}'`}
+                </p>
+              </div>
+              <section className={styles.billboard}>
+                {searched.map(
+                  ({ isAdult, anilistId, poster, totalEpisodes, title }) => (
+                    <Card
+                      key={anilistId}
+                      id={anilistId}
+                      image={poster}
+                      totalEpisodes={totalEpisodes}
+                      title={title}
+                      isAdult={isAdult}
+                    />
+                  )
+                )}
               </section>
             </section>
-          ) : (
-            <section className={styles.billboard_Wrapper}>
-              <section className={styles.billboard_Main}>
-                <div className={styles.billboard_HeaderMain}>
-                  <p className={styles.billboard_Header}>Trending</p>
-                  <Link href={`/search/trending`}>view all</Link>
-                </div>
-                <section className={styles.billboard}>
-                  {trending.map(
-                    ({ anilistId, poster, totalEpisodes, title, isAdult }) => {
-                      return (
-                        <Card
-                          key={anilistId}
-                          id={anilistId}
-                          image={poster}
-                          totalEpisodes={totalEpisodes}
-                          title={title}
-                          isAdult={isAdult}
-                        />
-                      );
-                    }
-                  )}
-                </section>
-              </section>
-              <section className={styles.billboard_Main}>
-                <div className={styles.billboard_HeaderMain}>
-                  <p className={styles.billboard_Header}>Popular</p>
-                  <Link href={`/search/popular`}>view all</Link>
-                </div>
-                <section className={styles.billboard}>
-                  {popular.map(
-                    ({ anilistId, poster, totalEpisodes, title, isAdult }) => {
-                      return (
-                        <Card
-                          key={anilistId}
-                          id={anilistId}
-                          image={poster}
-                          totalEpisodes={totalEpisodes}
-                          title={title}
-                          isAdult={isAdult}
-                        />
-                      );
-                    }
-                  )}
-                </section>
+          </section>
+        ) : (
+          <section className={styles.billboard_Wrapper}>
+            <section className={styles.billboard_Main}>
+              <div className={styles.billboard_HeaderMain}>
+                <p className={styles.billboard_Header}>Trending</p>
+                <Link href={`/search/trending`}>view all</Link>
+              </div>
+              <section className={styles.billboard}>
+                {trending.map(
+                  ({ anilistId, poster, totalEpisodes, title, isAdult }) => (
+                    <Card
+                      key={anilistId}
+                      id={anilistId}
+                      image={poster}
+                      totalEpisodes={totalEpisodes}
+                      title={title}
+                      isAdult={isAdult}
+                    />
+                  )
+                )}
               </section>
             </section>
-          )}
-        </section>
-        <Footer />
-      </Suspense>
+            <section className={styles.billboard_Main}>
+              <div className={styles.billboard_HeaderMain}>
+                <p className={styles.billboard_Header}>Popular</p>
+                <Link href={`/search/popular`}>view all</Link>
+              </div>
+              <section className={styles.billboard}>
+                {popular.map(
+                  ({ anilistId, poster, totalEpisodes, title, isAdult }) => (
+                    <Card
+                      key={anilistId}
+                      id={anilistId}
+                      image={poster}
+                      totalEpisodes={totalEpisodes}
+                      title={title}
+                      isAdult={isAdult}
+                    />
+                  )
+                )}
+              </section>
+            </section>
+          </section>
+        )}
+      </section>
+      <Footer />
     </>
   );
 }
