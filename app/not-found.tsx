2b65216839118e98d7e779affcd776008cced73c
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-08-04 10:47:59 +0600

    fix: deployment error on vercel

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,10 +6,10 @@ export default function NotFound() {
     <>
       <Navbar />
       <div className="e404">
-        <h1>Who's there!?</h1>
+        <h1>Who&#39;s there!?</h1>
         <img width={200} draggable="false" src="/angry_chan.png" alt="e404" />
-        <h1>Look where are you going.</h1>
-        <h1>Nothings here. Go back.</h1>
+        <h1>Look where you are going.</h1>
+        <h1>Nothing&#39;s here. Go back.</h1>
       </div>
       <Footer />
     </>
