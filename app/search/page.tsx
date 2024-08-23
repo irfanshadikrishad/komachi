aa62ec7fcd434e337971f56a536e9ea0ea4c45d0
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-08-24 04:34:35 +0600

    refactor

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -537,66 +537,70 @@ export default function Search() {
           </section>
         ) : (
           <section className={styles.billboard_Wrapper}>
-            <section className={styles.billboard_Main}>
-              <div className={styles.billboard_HeaderMain}>
-                <p className={styles.billboard_Header}>Trending</p>
-                <Link href={`/search/trending`}>view all</Link>
-              </div>
-              <section className={styles.billboard}>
-                {trending.map(
-                  ({
-                    anilistId,
-                    poster,
-                    sub_episodes = [],
-                    dub_episodes = [],
-                    totalEpisodes,
-                    title = { english: "", romaji: "" },
-                    isAdult,
-                  }) => (
-                    <Card
-                      key={anilistId}
-                      id={anilistId}
-                      image={poster}
-                      subCount={sub_episodes.length}
-                      dubCount={dub_episodes.length}
-                      totalCount={totalEpisodes}
-                      title={title.english ? title.english : title.romaji}
-                      isAdult={isAdult}
-                    />
-                  )
-                )}
+            {trending.length > 0 && (
+              <section className={styles.billboard_Main}>
+                <div className={styles.billboard_HeaderMain}>
+                  <p className={styles.billboard_Header}>Trending</p>
+                  <Link href={`/search/trending`}>view all</Link>
+                </div>
+                <section className={styles.billboard}>
+                  {trending.map(
+                    ({
+                      anilistId,
+                      poster,
+                      sub_episodes = [],
+                      dub_episodes = [],
+                      totalEpisodes,
+                      title = { english: "", romaji: "" },
+                      isAdult,
+                    }) => (
+                      <Card
+                        key={anilistId}
+                        id={anilistId}
+                        image={poster}
+                        subCount={sub_episodes.length}
+                        dubCount={dub_episodes.length}
+                        totalCount={totalEpisodes}
+                        title={title.english ? title.english : title.romaji}
+                        isAdult={isAdult}
+                      />
+                    )
+                  )}
+                </section>
               </section>
-            </section>
-            <section className={styles.billboard_Main}>
-              <div className={styles.billboard_HeaderMain}>
-                <p className={styles.billboard_Header}>Popular</p>
-                <Link href={`/search/popular`}>view all</Link>
-              </div>
-              <section className={styles.billboard}>
-                {popular.map(
-                  ({
-                    anilistId,
-                    poster,
-                    sub_episodes = [],
-                    dub_episodes = [],
-                    totalEpisodes,
-                    title = { english: "", romaji: "" },
-                    isAdult,
-                  }) => (
-                    <Card
-                      key={anilistId}
-                      id={anilistId}
-                      image={poster}
-                      subCount={sub_episodes.length}
-                      dubCount={dub_episodes.length}
-                      totalCount={totalEpisodes}
-                      title={title.english ? title.english : title.romaji}
-                      isAdult={isAdult}
-                    />
-                  )
-                )}
+            )}
+            {popular.length > 0 && (
+              <section className={styles.billboard_Main}>
+                <div className={styles.billboard_HeaderMain}>
+                  <p className={styles.billboard_Header}>Popular</p>
+                  <Link href={`/search/popular`}>view all</Link>
+                </div>
+                <section className={styles.billboard}>
+                  {popular.map(
+                    ({
+                      anilistId,
+                      poster,
+                      sub_episodes = [],
+                      dub_episodes = [],
+                      totalEpisodes,
+                      title = { english: "", romaji: "" },
+                      isAdult,
+                    }) => (
+                      <Card
+                        key={anilistId}
+                        id={anilistId}
+                        image={poster}
+                        subCount={sub_episodes.length}
+                        dubCount={dub_episodes.length}
+                        totalCount={totalEpisodes}
+                        title={title.english ? title.english : title.romaji}
+                        isAdult={isAdult}
+                      />
+                    )
+                  )}
+                </section>
               </section>
-            </section>
+            )}
           </section>
         )}
       </section>
