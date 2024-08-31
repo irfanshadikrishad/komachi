cdfa3117463a781f0056358f6f3f16dda9ecd392
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-08-31 19:57:27 +0600

    refactor: api

--- app/api/trending/route.ts
+++ app/api/trending/route.ts
@@ -5,19 +5,37 @@ export const POST = async (req: Request) => {
   try {
     await database();
     const { page = 1, perPage = 10 } = await req.json();
-    const results = await Anime.aggregate([
-      {
-        $addFields: {
-          anilistIdNumeric: { $toInt: "$anilistId" },
-        },
+
+    const query = `query ($page: Int, $perPage: Int) {
+                    Page(page: $page, perPage: $perPage) {
+                      media(type: ANIME, sort: TRENDING_DESC) {
+                        id
+                        title {
+                          romaji
+                          english
+                          native
+                          userPreferred
+                        }
+                      }
+                    }
+                  }`;
+
+    const request = await fetch(`https://graphql.anilist.co`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      {
-        $sort: { anilistIdNumeric: -1 },
-      },
-      {
-        $limit: perPage,
-      },
-    ]);
+      body: JSON.stringify({
+        query,
+        variables: { page, perPage },
+      }),
+    });
+
+    const { data } = await request.json();
+
+    const anilistIds = data?.Page?.media.map((item: { id: number }) => item.id);
+
+    const results = await Anime.find({ anilistId: { $in: anilistIds } });
 
     return new Response(JSON.stringify(results), {
       status: 200,
