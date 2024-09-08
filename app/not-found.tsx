e6544852bacf00101b54c297173d39e555bb64ea
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-09-09 02:42:14 +0600

    refactor: not_found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,10 +6,9 @@ export default function NotFound() {
     <>
       <Navbar />
       <div className="e404">
-        <h1>Who&#39;s there!?</h1>
-        <img width={200} draggable="false" src="/angry_chan.png" alt="e404" />
-        <h1>Look where you are going.</h1>
-        <h1>Nothing&#39;s here. Go back.</h1>
+        <img width={300} draggable="false" src="/not_found.png" alt="e404" />
+        <h1>404</h1>
+        <p>Page not found.</p>
       </div>
       <Footer />
     </>
