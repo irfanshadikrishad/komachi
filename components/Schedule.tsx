e40eca227d71a7a7ab24c2d898dad5f81ddf1360
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-09-21 08:59:33 +0600

    feat: schedule

---
+++ components/Schedule.tsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/schedule.module.css";
+
+export default function Schedule() {
+  return (
+    <section className={`container ${styles.schedule_Main}`}>
+      <p>Schedule</p>
+      <div>
+        <p>Fri</p>
+        <p>Sep 20</p>
+      </div>
+    </section>
+  );
+}
