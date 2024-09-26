203619d183990398723de72672134fdbb013211d
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-09-27 01:33:48 +0600

    feat: full rec. now viewable

--- components/Recommendations.tsx
+++ components/Recommendations.tsx
@@ -1,19 +1,41 @@
 import Card from "@/components/Card";
 import styles from "@/styles/recommendations.module.css";
 import { getTitle } from "@/utils/helpers";
+import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa6";
 
 export default function Recommendations({
   recommendations,
 }: {
   recommendations: any[];
 }) {
+  const [to, setTo] = useState(7);
+
   return (
     <section className={styles.wrapper}>
-      {recommendations.length > 0 && <h1>Recommendations</h1>}
+      {recommendations.length > 0 && (
+        <div className={styles.recHeader}>
+          <h1>Recommendations</h1>
+          <div>
+            <button
+              className={styles.downBtn}
+              onClick={() => {
+                if (to === 7) {
+                  setTo(recommendations.length);
+                } else {
+                  setTo(7);
+                }
+              }}
+            >
+              <FaChevronDown />
+            </button>
+          </div>
+        </div>
+      )}
       <div className={styles.cards}>
         {recommendations.length > 0 &&
           recommendations
-            .slice(0, 7)
+            .slice(0, to)
             .map(
               (
                 {
