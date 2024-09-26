94beb9faf4a5ed7b767b1b79b242c95df38c5f5c
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-09-26 23:11:45 +0600

    feat: page navigation for latest

--- app/api/recent/route.ts
+++ app/api/recent/route.ts
@@ -1,23 +1,38 @@
 import { database } from "@/utils/database";
 import Anime from "@/schema/anime";
 
-// Named export for the GET method
 export const POST = async (req: Request) => {
   try {
     await database();
     const { page = 1, perPage = 24 } = await req.json();
-    const all = await Anime.find({}).sort({ updatedAt: -1 }).limit(perPage);
+
+    // Calculate the number of documents to skip
+    const skip = (page - 1) * perPage;
+
+    const all = await Anime.find({})
+      .sort({ updatedAt: -1 })
+      .skip(skip)
+      .limit(perPage);
+
     const japan = await Anime.find({ origin: "JP" })
       .sort({ updatedAt: -1 })
+      .skip(skip)
       .limit(perPage);
+
     const china = await Anime.find({ origin: "CN" })
       .sort({ updatedAt: -1 })
+      .skip(skip)
       .limit(perPage);
+
     const korea = await Anime.find({ origin: "KR" })
       .sort({ updatedAt: -1 })
+      .skip(skip)
       .limit(perPage);
+
     return new Response(
       JSON.stringify({
+        page,
+        perPage,
         all,
         japan,
         china,
@@ -29,7 +44,7 @@ export const POST = async (req: Request) => {
       }
     );
   } catch (error) {
-    return new Response(JSON.stringify({ message: error }), {
+    return new Response(JSON.stringify({ message: error || error }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
